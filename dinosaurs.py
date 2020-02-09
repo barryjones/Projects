f2c01304226f065583689bb7a9baae5ed232a9b6
Barry Jones <me@barryjon.es>
2020-02-09 20:31:58 +1100

    done

--- dinosaurs.py
+++ dinosaurs.py
@@ -9,16 +9,6 @@
 # Write a program to read in the data files from disk, it must then print the names 
 # of only the bipedal dinosaurs from fastest to slowest. Do not print any other information. 
 
-# dinosaurs = [
-# {'NAME':'Hadrosaurus', 'LEG_LENGTH':'1.2', 'DIET':'herbivore', 'STRIDE_LENGTH':'1.4', 'STANCE':'bipedal'},
-# {'NAME':'Struthiomimus', 'LEG_LENGTH':'0.92', 'DIET':'omnivore', 'STRIDE_LENGTH':'1.34', 'STANCE':'bipedal'},
-# {'NAME':'Velociraptor', 'LEG_LENGTH':'1.0', 'DIET':'carnivore', 'STRIDE_LENGTH':'2.72', 'STANCE':'bipedal'}, 
-# {'NAME':'Euoplocephalus', 'LEG_LENGTH':'1.6', 'DIET':'herbivore', 'STRIDE_LENGTH':'1.87', 'STANCE':'quadrupedal'}, 
-# {'NAME':'Stegosaurus', 'LEG_LENGTH':'1.40', 'DIET':'herbivore', 'STRIDE_LENGTH':'1.90', 'STANCE':'quadrupedal'}, 
-# {'NAME':'Tyrannosaurus Rex', 'LEG_LENGTH':'2.5', 'DIET':'carnivore', 'STRIDE_LENGTH':'5.76', 'STANCE':'bipedal'}
-# ]
-
-
 str1 = """Hadrosaurus,1.2,herbivore
 Struthiomimus,0.92,omnivore
 Velociraptor,1.0,carnivore
@@ -26,70 +16,50 @@ Euoplocephalus,1.6,herbivore
 Stegosaurus,1.40,herbivore
 Tyrannosaurus Rex,2.5,carnivore"""
 
-#str1 = str1.replace('\n','')
+str2 = """Euoplocephalus,1.87,quadrupedal
+Stegosaurus,1.90,quadrupedal
+Tyrannosaurus Rex,5.76,bipedal
+Hadrosaurus,1.4,bipedal
+Struthiomimus,1.34,bipedal
+Velociraptor,2.72,bipedal"""
 
-lines = str1.split('\n')
-#print(lines)
-
-#dinosaurs = [] #you didn't tell me I was not using a dictionary!!!
 dinosaurs = dict()
 
+# 'read in' first sting and put into datastructure
+lines = str1.split('\n')
 
 for line in lines:
-	items = line.split(',')
-	#print(items)
-	
+	items = line.split(',')	
 	dinosaurs[items[0]] = {
 		'LEG_LENGTH':items[1],
 		'DIET':items[2]
 	}
 
-print(dinosaurs)
-
-
-
-str2 = """Euoplocephalus,1.87,quadrupedal 
-Stegosaurus,1.90,quadrupedal 
-Tyrannosaurus Rex,5.76,bipedal 
-Hadrosaurus,1.4,bipedal 
-Struthiomimus,1.34,bipedal 
-Velociraptor,2.72,bipedal"""
-
+# 'read in' second string and append to datastructure
 lines = str2.split('\n')
+
 for line in lines:
 	items = line.split(',')
-	#print(items)
-	dinosaurs[items[0]]['STRIDE_LENGTH'] = items[1] #these two lines took some thinking!
+	dinosaurs[items[0]]['STRIDE_LENGTH'] = items[1]
 	dinosaurs[items[0]]['STANCE'] = items[2]
-	
- 	
-print(dinosaurs)
 
-#find item in dinosaurs dict and then append extra information
+# workout speed of bipedal dinosaurs and put into list
+bipedalDinosaurs = []
 
-#find item in dinosaurs
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# dinoSpeed = []
-
-# for i in dinosaurs:
-# 	if i["STANCE"] == "bipedal":
-# 		STRIDE_LENGTH = float(i["STRIDE_LENGTH"])
-# 		LEG_LENGTH = float(i["LEG_LENGTH"])
-# 		speed = ((STRIDE_LENGTH / LEG_LENGTH) - 1) * ((LEG_LENGTH * 9.8)**(1/2))
-# 		dinoSpeed.append({'NAME':i['NAME'], 'SPEED':speed})
+for name in dinosaurs:
+	if dinosaurs[name]['STANCE'] == "bipedal":
 		
+		STRIDE_LENGTH = float(dinosaurs[name]['STRIDE_LENGTH'])
+		LEG_LENGTH = float(dinosaurs[name]['LEG_LENGTH'])
+		speed = ((STRIDE_LENGTH / LEG_LENGTH) - 1) * ((LEG_LENGTH * 9.8)**(1/2))
 
-#print(dinoSpeed)
+		bipedalDinosaurs.append([name,speed])
+
+# sort fastest to slowest
+bipedalDinosaurs.sort(key=lambda x: x[1], reverse=True)
+
+# print only the names
+for i in bipedalDinosaurs:
+	print(i[0])
+
+
